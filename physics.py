01c96e998ddb13e98ae16ff288e24a18d55bb74d
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-05-24 14:26:54 +0600

    Physics - Implemented Ballpath

--- physics.py
+++ physics.py
@@ -1,1 +1,15 @@
 import math
+
+
+def ballPath(startx, starty, power, ang, time):
+    angle = ang
+    velx = math.cos(angle) * power
+    vely = math.sin(angle) * power
+
+    distX = velx * time
+    distY = (vely * time) + ((-9.8 * (time ** 2)) / 2)
+
+    newx = round(distX + startx)
+    newy = round(starty - distY)
+
+    return (newx, newy)
