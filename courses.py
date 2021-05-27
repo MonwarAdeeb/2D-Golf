674ad8f17dbe661941e428a44024ae51ce35e323
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-05-27 11:14:54 +0600

    Initialized Courses

---
+++ courses.py
@@ -0,0 +1,11 @@
+from tkinter import messagebox
+import tkinter as tk
+from time import sleep, time
+import startScreen
+import courses
+import math
+import physics
+import subprocess
+import sys
+import get_pip
+import os
