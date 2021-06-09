430e51ec74b935f38698dff34149fc3028da5d3f
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-06-10 00:09:42 +0600

    Worked on StartScreen

--- startScreen.py
+++ startScreen.py
@@ -3,3 +3,11 @@ import os
 import tkinter as tk
 from tkinter import messagebox
 import sys
+
+pygame.init()
+
+win = pygame.display.set_mode((1080, 600))
+title = pygame.image.load(os.path.join('img', 'title.png'))
+back = pygame.image.load(os.path.join('img', 'back.png'))
+course = pygame.image.load(os.path.join('img', 'course1.png'))
+course1 = pygame.transform.scale(course, (200, 200))
