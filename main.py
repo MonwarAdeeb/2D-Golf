2b8935edfcbef22100eac9e863797a9b39e30198
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-06-24 10:37:52 +0600

    Worked on Main File

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from tkinter import messagebox
+import tkinter as tk
+from time import sleep, time
+import startScreen
+import courses
+import math
+import physics
 import subprocess
 import sys
 import get_pip
@@ -6,3 +13,10 @@ import os
 
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+
+try:
+    print("[GAME] Trying to import pygame")
+    import pygame
+except:
+    print("[EXCEPTION] Pygame not installed")
