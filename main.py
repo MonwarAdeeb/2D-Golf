548afb73433c6903d9fc8ebc816fb4de9855634e
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-07-05 18:27:24 +0600

    Worked on Main File

--- main.py
+++ main.py
@@ -197,3 +197,14 @@ class scoreSheet():
                     str(self.parList[i - 2]), 1, (128, 128, 128))
                 self.win.blit(blit, (startx + 60 + 133, starty +
                                      10 + ((i - 1) * (self.height/10))))
+                try:  # Catch the index out of range error, display the stokes each level
+                    if self.strokes[i - 2] < self.parList[i - 2]:
+                        color = (0, 166, 0)
+                    elif self.strokes[i - 2] > self.parList[i - 2]:
+                        color = (255, 0, 0)
+                    else:
+                        color = (0, 0, 0)
+
+                    blit = self.font.render(str(self.strokes[i - 2]), 1, color)
+                    self.win.blit(
+                        blit, ((startx + 60 + 266, starty + 10 + ((i - 1) * (self.height/10)))))
