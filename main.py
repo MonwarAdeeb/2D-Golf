cbf1991e600b103445a1754d75ffe7759973e9b1
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-07-24 11:36:37 +0600

    Worked on Main File

--- main.py
+++ main.py
@@ -453,5 +453,3 @@ def redrawWindow(ball, line, shoot=False, update=True):
 
     # Draw information such as strokes, par and powerups left
     smallFont = pygame.font.SysFont('comicsansms', 20)
-    text = smallFont.render('Left: ' + str(powerUps), 1, (64, 64, 64))
-    win.blit(text, (920, 55))
