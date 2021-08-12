c242cfe4ea0aa01b2b62702cde9034ef53bc8c43
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-08-12 11:39:24 +0600

    Worked on Main File

--- main.py
+++ main.py
@@ -611,3 +611,8 @@ while starting:
             if startScreen.click(pos) != None:
                 starting = False
                 break
+            if startScreen.shopClick(pos) == True:
+                surface = startScreen.drawShop()
+                win.blit(surface, (0, 0))
+                pygame.display.update()
+                shop = True
